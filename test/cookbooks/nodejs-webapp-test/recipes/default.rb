2e427e35d14c63d699fd1697ea5ec26dc35a0d43
Jeffrey Borcean <jeffrey@osuosl.org>
2015-08-25 16:04:31 -0700

    Remove addition to default test recipie

--- test/cookbooks/nodejs-webapp-test/recipes/default.rb
+++ test/cookbooks/nodejs-webapp-test/recipes/default.rb
@@ -42,7 +42,6 @@ nodejs_webapp 'test_a' do
   owner 'test_a'
   group 'test_a'
 
-  name 'custom'
   script 'run.js'
   repository 'https://github.com/osuosl/nodejs-test-apps.git'
   branch 'custom'
