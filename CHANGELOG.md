500dc5fc2716da8336fcda6d2e8b70135019e827
Evan Tschuy <tschuy@osuosl.org>
2015-08-25 09:31:06 -0700

    Initial commit of nodejs-webapp

---
+++ CHANGELOG.md
@@ -0,0 +1,9 @@
+nodejs-webapp CHANGELOG
+=======================
+This file is used to list changes made in each version of the
+nodejs-webapp cookbook.
+
+0.1.0
+-----
+- Initial release of nodejs-webapp
+
---
+++ metadata.rb
@@ -0,0 +1,8 @@
+name             'nodejs-webapp'
+maintainer       'Oregon State University'
+maintainer_email 'chef@osuosl.org'
+license          'apache2'
+description      'Installs/Configures nodejs-webapp'
+long_description 'Installs/Configures nodejs-webapp'
+version          '0.1.0'
+
