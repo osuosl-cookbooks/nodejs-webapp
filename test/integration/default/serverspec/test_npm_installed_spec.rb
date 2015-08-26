344f3ddcc6edf44f89155187ef8fb9a6776e3b0f
Jeffrey Borcean <jjborcean@gmail.com>
2015-08-26 11:39:15 -0700

    NPM install package test

--- test/integration/default/serverspec/test_npm_installed_spec.rb
+++ test/integration/default/serverspec/test_npm_installed_spec.rb
@@ -4,6 +4,4 @@ set :backend, :exec
 # Test that express was properly installed into node_modules
 describe file('/opt/custom/source/node_modules/express') do
   it { should be_directory }
-  it { should be_grouped_into 'root' }
-  it { should be_owned_by 'root' }
 end
