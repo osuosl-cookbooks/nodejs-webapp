95af071e059328ecc96bbf6629cbc42261cddf32
Tristan Patch <patcht@osuosl.org>
2016-09-28 09:38:02 -0700

    Fix rubocop issue.

--- test/integration/default/serverspec/test_tmp_clear_spec.rb
+++ test/integration/default/serverspec/test_tmp_clear_spec.rb
@@ -3,6 +3,6 @@ set :backend, :exec
 
 # Test that the git repository's directory is properly set up
 describe command('ls /tmp | grep "npm-"') do
-  its(:stdout) { should match "" }
+  its(:stdout) { should match '' }
   its(:exit_status) { should eq 0 }
 end
