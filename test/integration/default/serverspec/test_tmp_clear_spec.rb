ab6ea0623c5f464e71427c4017f1f961eb3733c2
Tristan Patch <patcht@osuosl.org>
2016-09-28 09:30:05 -0700

    Add tests.

---
+++ test/integration/default/serverspec/test_tmp_clear_spec.rb
@@ -0,0 +1,8 @@
+require 'serverspec'
+set :backend, :exec
+
+# Test that the git repository's directory is properly set up
+describe command('ls /tmp | grep "npm-"') do
+  its(:stdout) { should match "" }
+  its(:exit_status) { should eq 0 }
+end
