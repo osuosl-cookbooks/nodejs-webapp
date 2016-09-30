918035650f939197951be95eb5ad778e8c4d5f65
Tristan Patch <patcht@osuosl.org>
2016-09-30 12:08:00 -0700

    Use correct grep exit status.

--- test/integration/default/serverspec/test_tmp_clear_spec.rb
+++ test/integration/default/serverspec/test_tmp_clear_spec.rb
@@ -1,8 +1,8 @@
 require 'serverspec'
 set :backend, :exec
 
-# Test that the git repository's directory is properly set up
+# Test that the tmp folder has been cleared of unnecessary files
 describe command('ls /tmp | grep "npm-"') do
   its(:stdout) { should match '' }
-  its(:exit_status) { should eq 0 }
+  its(:exit_status) { should eq 1 } # 1 = no lines match
 end
